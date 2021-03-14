64b4b19de281b90ed0d48a6578a653788ec54f97
Bravo <gmb2388@yahoo.com>
2021-03-13 17:27:32 -0800

    models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    product_id: sequelize.INTEGER,
+    tag_id: sequelize.INTEGER
   },
   {
     sequelize,
